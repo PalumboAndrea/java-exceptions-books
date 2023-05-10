6c009db6a14808a1d01bca953dfe396fb6603660
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-05-10 15:54:24 +0200

    add all functionalities and structure

--- src/org/java/exceptions/books/Libro.java
+++ src/org/java/exceptions/books/Libro.java
@@ -1,5 +1,69 @@
 package org.java.exceptions.books;
 
 public class Libro {
+	
+	private String title;
+	private int pageNumber;
+	private String author;
+	private String editor;
+	
+	public Libro (String title, int pageNumber,
+			String author, String editor) throws Exception {
+		setTitle(title);
+		setPageNumber(pageNumber);
+		setAuthor(author);
+		setEditor(editor);
+	}
 
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) throws Exception {
+
+		if (title.length() == 0) {
+			throw new Exception("Il titolo non puo' essere vuoto");
+		}
+		
+		this.title = title;
+	}
+	public int getPageNumber() {
+		return pageNumber;
+	}
+	public void setPageNumber(int pageNumber) throws Exception {
+		
+		if (pageNumber <= 0) {
+			throw new Exception("Le pagine devono essere un numero positivo");
+		}
+		
+		this.pageNumber = pageNumber;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getEditor() {
+		return editor;
+	}
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+	
+	protected String getBookString() {
+		
+		return "Titolo: " + getTitle()
+			+ "\nAutore: " + getAuthor()
+			+ "\nEditore: " + getEditor();
+	}
+	
+	@Override
+	public String toString() {
+		
+		return getBookString();
+	}
+	
+	
+	
+	
 }
---
+++ src/org/java/exceptions/books/Main.java
@@ -0,0 +1,69 @@
+package org.java.exceptions.books;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	private static final String FILENAME = "/Users/andre/Desktop/Scuola/prova.txt";
+	
+	public static void main(String[] args) throws IOException {
+		
+		FileWriter myWriter = new FileWriter(FILENAME);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quanti libri vuoi inserire?");
+		int bookNumber = sc.nextInt();
+		sc.nextLine();
+		
+		int index = -1;
+		
+		Libro[] lArr = new Libro[bookNumber];
+		
+		while (index < bookNumber - 1) {
+			index ++;
+			
+			try {
+				
+				System.out.println("Qual è il titolo? (Non si accetta un campo vuoto)");
+				String title = sc.nextLine();
+				System.out.println("Qual è il numero di pagine? (>0)");
+				int pageNumber = sc.nextInt();
+				sc.nextLine();
+				System.out.println("Chi è l'autore?");
+				String author = sc.nextLine();
+				System.out.println("Chi è l'editore?");
+				String editor = sc.nextLine();
+				
+				Libro l = new Libro(title, pageNumber, author, editor);
+				
+				lArr[index] = l;
+			} catch (Exception e){
+				
+				System.err.println("Errore: \n" + e.getMessage());
+			}
+			
+		}
+		
+		for (int i = 0; i < lArr.length; i++) {
+			myWriter.write("\n" + lArr[i].toString() + "\n");
+		}
+		
+		myWriter.close();
+		
+		File mioFile = new File(FILENAME);
+		Scanner reader = new Scanner(mioFile);
+		System.out.println("\nLa lettura del file creato è:");
+		
+		while (reader.hasNextLine()) {
+			
+			String readerBook = reader.nextLine();
+			System.out.println(readerBook);
+		}
+		
+		sc.close();
+		reader.close();
+		
+	}
+}
